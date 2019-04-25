cccc9e2604efabe53480de3208760ab600c8cb0b
Christopher Morgan <cjmorgan1185@gmail.com>
2019-04-25 22:55:09 +0000

    updated log

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -83,26 +83,11 @@ WSGI_APPLICATION = 'issue_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
-if "DATABASE_URL" in os.environ:
 DATABASES = {
     'default':dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
-else:
-    print("POSTGRES URL not found using sqlite instead")
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
