e28bcae0b8462de21ac4e6b31f54bf6621a5456b
Christopher Morgan <cjmorgan1185@gmail.com>
2019-04-25 22:40:27 +0000

    corrected for deployment

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+heroku config:set DISABLE_COLLECTSTATIC=1
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "issue_tracker.settings")
     try:
